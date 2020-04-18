5ebc21f136c371b32985fc66c3d530887d0d71d1
Laura Groettrup <laura.groettrup@edu.fh-joanneum.at>
2020-04-18 20:09:33 +0200

    add: more screens

--- FrontendImplementation/FileBuddyMobileClient/app/src/main/java/com/fh/joanneum/filebuddymobileclient/MainActivity.kt
+++ FrontendImplementation/FileBuddyMobileClient/app/src/main/java/com/fh/joanneum/filebuddymobileclient/MainActivity.kt
@@ -2,11 +2,42 @@ package com.fh.joanneum.filebuddymobileclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.SimpleAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.list_holder)
+
+        var resultsListView: ListView  = findViewById (R.id.results_listview)
+
+        var nameAddresses: HashMap<String, String> = HashMap()
+        nameAddresses.put("Tyga", "343 Rack City Drive");
+        nameAddresses.put("Rich Homie Quan", "111 Everything Gold Way");
+        nameAddresses.put("Donna", "789 Escort St");
+        nameAddresses.put("Bartholomew", "332 Dunkin St");
+        nameAddresses.put("Eden", "421 Angelic Blvd");
+
+        var listElements: List<HashMap<String, String>> = ArrayList();
+        var adapter: SimpleAdapter = SimpleAdapter(this, listElements, R.layout.list_element,
+            String[]{"First Line", "Second Line"},
+            int[]{R.id.text1, R.id.text2})
+
+
+        Iterator it = nameAddresses.entrySet().iterator();
+        while (it.hasNext())
+        {
+            HashMap<String, String> resultsMap = new HashMap<>();
+            Map.Entry pair = (Map.Entry)it.next();
+            resultsMap.put("First Line", pair.getKey().toString());
+            resultsMap.put("Second Line", pair.getValue().toString());
+            listItems.add(resultsMap);
+        }
+
+        resultsListView.setAdapter(adapter);
+
+    }
     }
 }
