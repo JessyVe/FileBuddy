dcef252d258122b7080745aab0a1e568a1cb39d6
Jessica Veit <jessica.veit@edu.fh-joanneum.at>
2020-04-19 08:50:57 +0200

    [Frontend - C#] Initial commit

---
+++ FrontendImplementation/FileBuddyDesktopClient/FileBuddyUI/Views/MainOverview.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FileBuddyUI.Views
+{
+    /// <summary>
+    /// Interaction logic for MainOverview.xaml
+    /// </summary>
+    public partial class MainOverview : Window
+    {
+        public MainOverview()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClose(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }
+    }
+}
