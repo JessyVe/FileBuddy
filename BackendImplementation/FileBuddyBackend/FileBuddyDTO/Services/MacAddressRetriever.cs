a57c004121090b19ed9ba6303d0f73a8160da60b
Jessica Veit <jessica.veit@edu.fh-joanneum.at>
2020-04-21 08:04:52 +0200

    [C# - Backend] Add mac address retriever

---
+++ BackendImplementation/FileBuddyBackend/FileBuddyDTO/Services/MacAddressRetriever.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.NetworkInformation;
+
+namespace SharedRessources.Services
+{
+    public static class MacAddressRetriever
+    {
+        private static readonly log4net.ILog Log 
+            = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Returns the MAC address of the NIC with maximum speed.
+        /// </summary>
+        /// <returns>The MAC address.</returns>
+        public static string GetMacAddress()
+        {
+            const int MIN_MAC_ADDR_LENGTH = 12;
+            string macAddress = string.Empty;
+            long maxSpeed = -1;
+
+            foreach (var nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                Log.Debug(
+                    "Found MAC Address: " + nic.GetPhysicalAddress() +
+                    " Type: " + nic.NetworkInterfaceType);
+
+                string tempMac = nic.GetPhysicalAddress().ToString();
+                if (nic.Speed > maxSpeed &&
+                    !string.IsNullOrEmpty(tempMac) &&
+                    tempMac.Length >= MIN_MAC_ADDR_LENGTH)
+                {
+                    Log.Debug("New Max Speed = " + nic.Speed + ", MAC: " + tempMac);
+                    maxSpeed = nic.Speed;
+                    macAddress = tempMac;
+                }
+            }
+            return macAddress;
+        }
+    }
+}
