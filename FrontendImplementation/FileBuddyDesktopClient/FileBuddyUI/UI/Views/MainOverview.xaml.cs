4e3001a6086d9de049d87c0ffeaee137c940ba9a
Jessica Veit <jessica.veit@edu.fh-joanneum.at>
2020-04-25 10:32:01 +0200

    [C# - Frontend] Implement drag-and-drop functionality

--- FrontendImplementation/FileBuddyDesktopClient/FileBuddyUI/UI/Views/MainOverview.xaml.cs
+++ FrontendImplementation/FileBuddyDesktopClient/FileBuddyUI/UI/Views/MainOverview.xaml.cs
@@ -22,9 +22,45 @@ namespace FileBuddyUI.UI.Views
             InitializeComponent();
         }
 
+        private void File_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);     
+            }
+            ResetView();
+        }
+
+        private void cDragArea_DragEnter(object sender, DragEventArgs e)
+        {
+            lbDragNDrop.Visibility = Visibility.Visible;
+            tabControl.Visibility = Visibility.Hidden;
+        }
+
+        private void cDragArea_DragLeave(object sender, DragEventArgs e)
+        {
+            ResetView();
+        }
+
+        private void ResetView()
+        {
+            lbDragNDrop.Visibility = Visibility.Hidden;
+            tabControl.Visibility = Visibility.Visible;
+        }    
+
         private void OnClose(object sender, RoutedEventArgs e)
         {
             Application.Current.MainWindow.Close();
+        }
+
+        private void OnWindowMinimize(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void OnWindowMaximize(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
         }
     }
 }
