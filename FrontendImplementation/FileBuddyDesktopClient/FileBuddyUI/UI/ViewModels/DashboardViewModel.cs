290e1d289bdcd52f968de278ce72c32e0b26c5c9
Jessica Veit <jessica.veit@edu.fh-joanneum.at>
2020-05-14 21:07:55 +0200

    [Frontend - C#] Add data binding for dashboard

--- FrontendImplementation/FileBuddyDesktopClient/FileBuddyUI/UI/ViewModels/DashboardViewModel.cs
+++ FrontendImplementation/FileBuddyDesktopClient/FileBuddyUI/UI/ViewModels/DashboardViewModel.cs
@@ -1,8 +1,35 @@
 ﻿using FileBuddyUI.UI.Helper;
+using SharedRessources.Dtos;
+using System;
+using System.Collections.ObjectModel;
 
 namespace FileBuddyUI.UI.ViewModels
 {
     public class DashboardViewModel : ViewModelBase
     {
+        public ObservableCollection<SharedFile> ReceivedFiles { get; set; }
+        public ObservableCollection<SharedFile> SentFiles { get; set; }
+
+        public DashboardViewModel()
+        {
+            ReceivedFiles = new ObservableCollection<SharedFile>();
+            SentFiles = new ObservableCollection<SharedFile>();
+            GenerateDemoData();
+        }
+
+        private void GenerateDemoData()
+        {
+            ReceivedFiles.Add(new SharedFile()
+            {
+                SharedFileName = "Received.txt", 
+                UploadDate = DateTime.Now
+            });
+
+            SentFiles.Add(new SharedFile()
+            {
+                SharedFileName = "Sent.txt",
+                UploadDate = DateTime.Now
+            });
+        }
     }
 }
