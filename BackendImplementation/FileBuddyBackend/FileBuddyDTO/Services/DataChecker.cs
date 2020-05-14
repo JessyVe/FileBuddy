d81280cecfe6a033f57c86190c91fe7fbf809d66
Jessica Veit <jessica.veit@edu.fh-joanneum.at>
2020-05-14 17:37:53 +0200

    [Backend - C#] Add mail validation

---
+++ BackendImplementation/FileBuddyBackend/FileBuddyDTO/Services/DataChecker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace SharedRessources.Services
+{
+    public static class DataChecker
+    {
+        public static bool IsValid(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
