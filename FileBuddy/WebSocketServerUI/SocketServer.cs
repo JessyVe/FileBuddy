706d88f6e4b2562bf4ab7637a35931ecfaeb5253
Jessica Veit <jessica.veit@edu.fh-joanneum.at>
2020-06-19 14:06:04 +0200

    [Backend - C#] Code clean up and logging

--- FileBuddy/WebSocketServerUI/SocketServer.cs
+++ FileBuddy/WebSocketServerUI/SocketServer.cs
@@ -7,6 +7,9 @@ namespace WebSocketServerUI
 {
     public class SocketServer : ServerBase
     {
+        private static readonly log4net.ILog Log =
+                 log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public SocketServer(IPAddress address, int port) : base(address, port)
         { }
 
@@ -17,19 +20,31 @@ namespace WebSocketServerUI
         /// <param name="client"></param>
         protected override void ProcessClientPackets(object receivedMessage, SocketClient client)
         {
-            if(receivedMessage is UserConnectionMessage userConnectionPacket)
+            if(receivedMessage is UserConnectionMessage userConnectionMessage)
             {
-                client.UserId = userConnectionPacket.UserId;
+                Log.Debug("userConnectionMessage was received. UserId will be updated.");
+                client.UserId = userConnectionMessage.UserId;
             }
             else if (receivedMessage is PingMessage pingPacket)
             {
+                Log.Debug("PingMessage was received. Answering client...");
                 client.SendObject(pingPacket).Wait();
+                Log.Debug("Ping succeeded!");
             }
             else if(receivedMessage is UpdateMessage updateMessage)
             {
+                Log.Debug("UpdateMessage was received. Affected clients will be notified.");
                 var user = Connections.Find(connection => connection.UserId == updateMessage.ReceiverId);
+
                 if (user != null)
+                {
                     client.SendObject(new UpdateMessage()).Wait();
+                    Log.Debug("Successfully notified client.");
+                }
+                else
+                {
+                    Log.Debug("Client seems to be offline. Realtime update was not possible.");
+                }
             }
         }
     }
